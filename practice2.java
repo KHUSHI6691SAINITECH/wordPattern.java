13be33aa3110ed10c3197ddc7c071bcf215def0d
khushi <khushi9389saini@gmail.com>
2023-08-05 05:28:43 -0700

    Initial commit

---
+++ practice2.java
@@ -0,0 +1,12 @@
+
+public class question3 {
+   public static void main(String[] agrs) {
+	   int n= 5;
+	   for(int i=1; i<n; i++) {
+		   for( int j=1; j<n; j++) {
+			   System.out.print(i);
+		   }
+		   System.out.println();
+	   }
+   }
+}
